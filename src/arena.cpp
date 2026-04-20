83e95938c05866ab4fffd6be2221a2c1672122d6
miriamdaily <miriamthegreat123@gmail.com>
2026-04-19 22:02:54 -0700

    move logic to arena

--- src/arena.cpp
+++ src/arena.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "arena.h"
+#include <cstdlib>
 
 void Arena::battle(Creature &a, Creature &b)
 {
@@ -12,25 +13,35 @@ void Arena::battle(Creature &a, Creature &b)
         std::cout << "\nTurn " << turn << std::endl;
 
         std::cout << a.name << " attacks!" << std::endl;
-        a.attack(b);
+
+        int hitA = a.damage + (std::rand() % 3 - 1);
+        if (hitA < 0) hitA = 0;
+
+        b.health -= hitA;
+        if (b.health < 0) b.health = 0;
+
+        std::cout << a.name << " deals " << hitA << " damage!" << std::endl;
         std::cout << b.name << " health: " << b.health << std::endl;
 
         if (!b.isAlive())
             break;
 
         std::cout << b.name << " attacks!" << std::endl;
-        b.attack(a);
+
+        int hitB = b.damage + (std::rand() % 3 - 1);
+        if (hitB < 0) hitB = 0;
+
+        a.health -= hitB;
+        if (a.health < 0) a.health = 0;
+
+        std::cout << b.name << " deals " << hitB << " damage!" << std::endl;
         std::cout << a.name << " health: " << a.health << std::endl;
 
         turn++;
     }
 
     if (a.isAlive())
-    {
         std::cout << a.name << " wins!" << std::endl;
-    }
     else
-    {
         std::cout << b.name << " wins!" << std::endl;
-    }
 }
